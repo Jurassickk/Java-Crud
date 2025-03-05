7b44c8cf8539eeef0061e861816eca01cc4b2653
Jurassickk <cuellardiego310@gmail.com>
2025-03-05 13:36:49 -0500

    Front end

---
+++ src/main/java/com/sena/crud_basic/model/Categories.java
@@ -0,0 +1,41 @@
+package com.sena.crud_basic.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity(name = "categories")
+public class Categories {
+
+    @Id
+    @Column(name = "category_id")
+    private int category_id;
+
+    @Column(name = "name", length = 50, nullable = false)
+    private String name;
+
+    public Categories() {
+    }
+
+    public Categories(int category_id, String name) {
+        this.category_id = category_id;
+        this.name = name;
+    }
+
+    public int getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(int category_id) {
+        this.category_id = category_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/main/java/com/sena/crud_basic/model/Products.java
@@ -0,0 +1,68 @@
+package com.sena.crud_basic.model;
+
+import java.text.DecimalFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity(name = "products")
+public class Products {
+
+    @Id
+    @Column(name = "product_id")
+    private int product_id;
+
+    @Column(name = "name", length = 50, nullable = false)
+    private String name;
+
+    @Column(name = "type", length = 50, nullable = false)
+    private String type;
+
+    @Column(name = "price", nullable = false)
+    private DecimalFormat price;
+
+    public Products() {
+
+    }
+
+    public Products(int product_id, String name, String type, DecimalFormat price) {
+        this.product_id = product_id;
+        this.name = name;
+        this.type = type;
+        this.price = price;
+    }
+
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public DecimalFormat getPrice() {
+        return price;
+    }
+
+    public void setPrice(DecimalFormat price) {
+        this.price = price;
+    }
+
+}
